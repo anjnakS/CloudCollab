c42c579b659e58c62e0630e5da18c41a882c40d6
anjnakS <anjnakhanna69@gmail.com>
2022-02-15 16:45:09 +0000

    Added Login

--- Collaborator/src/App.js
+++ Collaborator/src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from './Components/Dashboard';
 import Treeview from './Components/Treeview';
+import Login from './Components/Login';
+
 
 function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
       <BrowserRouter>
         <Switch>
-          <Route path="/dashboard">
+          <Route path="/">
             <Dashboard />
           </Route>
           <Route path="/collab">
--- Collaborator/src/index.js
+++ Collaborator/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { StyledEngineProvider } from '@mui/material/styles';
 
 
@@ -13,8 +12,3 @@ ReactDOM.render(
   document.querySelector("#root")
 );
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
