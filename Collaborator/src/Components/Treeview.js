de0ff5165129d07c51e9a9528fc566a812dca08a
anjnakS <anjnakhanna69@gmail.com>
2022-02-23 22:05:43 +0000

    Added Context Menu

--- Collaborator/src/Components/Treeview.js
+++ Collaborator/src/Components/Treeview.js
@@ -3,11 +3,35 @@ import TreeView from '@mui/lab/TreeView';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import TreeItem from '@mui/lab/TreeItem';
+import { MenuList, MenuItem} from '@mui/material';
+import { makeStyles } from "@material-ui/core/styles";
+import { ClickAwayListener, Fade } from "@material-ui/core";
+import { Paper , Popper} from '@mui/material'
 import Team from './Team';
 import TeamGBC from './TeamGBC';
 import Games from './Games';
 import Trainings from './Trainings';
+
+/* copied from https://github.com/mui-org/material-ui/blob/v4.3.2/packages/material-ui/src/Menu/Menu.js#L21 */
+const useMenuStyles = makeStyles({
+  /* Styles applied to the `Paper` component. */
+  paper: {
+    // specZ: The maximum height of a simple menu should be one or more rows less than the view
+    // height. This ensures a tapable area outside of the simple menu with which to dismiss
+    // the menu.
+    maxHeight: "calc(100% - 96px)",
+    // Add iOS momentum scrolling.
+    WebkitOverflowScrolling: "touch"
+  },
+  /* Styles applied to the `List` component via `MenuList`. */
+  list: {
+    // We disable the focus ring for mouse, touch and keyboard users.
+    outline: 0
+  }
+});
 export default function Treeview() {
+
+  
   const data= {
     id: "company",
     name: "Shareworks by Morgan Stanley",
@@ -124,6 +148,32 @@ const renderTree = (nodes) => (
       : null}
   </TreeItem>
 );
+const [open, setOpen] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleContextMenu = (e) => {
+    e.preventDefault();
+    const { clientX, clientY } = e;
+    setOpen(true);
+    const virtualElement = {
+      getBoundingClientRect: () => ({
+        width: 0,
+        height: 0,
+        top: clientY,
+        right: clientX,
+        bottom: clientY,
+        left: clientX
+      })
+    };
+    setAnchorEl(virtualElement);
+  };
+
+  const id = open ? "faked-reference-popper" : undefined;
+  const menuClasses = useMenuStyles();
   return(
   
     <div style={{ display: 'flex', padding: 30 }}>
@@ -153,12 +203,34 @@ const renderTree = (nodes) => (
       
        width: window.innerWidth * 0.35,
      }}> <Team/><br/><TeamGBC/></div>
-      <div  style={{
+      <div onContextMenu={handleContextMenu} style={{
       
       width: window.innerWidth * 0.35,
      
      
-     }} > </div>
+     }} >
+        <Popper
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        transition
+        placement="bottom-start"
+      >
+        {({ TransitionProps }) => (
+          <ClickAwayListener onClickAway={handleClose}>
+            <Fade {...TransitionProps}>
+              <Paper className={menuClasses.paper}>
+                <MenuList className={menuClasses.list} autoFocus>
+                  <MenuItem onClick={handleClose}>Create Outlook Meeting</MenuItem>
+                  <MenuItem onClick={handleClose}>Send Teams channel message</MenuItem>
+                  <MenuItem onClick={handleClose}>Get calendar for the week</MenuItem>
+                </MenuList>
+              </Paper>
+            </Fade>
+          </ClickAwayListener>
+        )}
+      </Popper>
+        </div>
      <div > <Trainings/> </div>
      <br/><br/>
   <div > <Games/> </div>
