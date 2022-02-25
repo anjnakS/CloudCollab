d32cce35842b53cfc86a9142b78463566f487678
anjnakS <anjnakhanna69@gmail.com>
2022-02-25 01:22:35 +0000

    Added Signin

--- Collaborator/src/App.js
+++ Collaborator/src/App.js
@@ -1,16 +1,57 @@
 import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
+import { loginRequest } from "./authConfig";
+import { PageLayout } from "./Components/PageLayout";
+import { ProfileData } from "./Components/ProfileData";
+import { callMsGraph } from "./graph";
+import Button from "react-bootstrap/Button";
 
 import Treeview from './Components/Treeview';
 import JiraResults from './Components/JiraResults';
 
 import SearchBar from './Search';
-function App() {
 
-  return (
-    <div className="wrapper">
+
+
+/**
+ * Renders information about the signed-in user or a button to retrieve data about the user
+ */
+const ProfileContent = () => {
+    const { instance, accounts } = useMsal();
+    const [graphData, setGraphData] = useState(null);
+
+    function RequestProfileData() {
+        // Silently acquires an access token which is then attached to a request for MS Graph data
+        instance.acquireTokenSilent({
+            ...loginRequest,
+            account: accounts[0]
+        }).then((response) => {
+            callMsGraph(response.accessToken).then(response => setGraphData(response));
+        });
+    }
+
+    return (
+        <>
+            <h5 className="card-title">Welcome {accounts[0].name}</h5>
+            {graphData ? 
+                <ProfileData graphData={graphData} />
+                :
+                <Button variant="secondary" onClick={RequestProfileData}>Request Profile Information</Button>
+            }
+        </>
+    );
+};
+
+/**
+ * If a user is authenticated the ProfileContent component above is rendered. Otherwise a message indicating a user is not authenticated is rendered.
+ */
+const MainContent = () => {    
+    return (
+        <div className="App">
+            <AuthenticatedTemplate>
+            <div className="wrapper">
       <h1>Cloud Collaborators</h1>
      
     
@@ -28,7 +69,19 @@ function App() {
         </Switch>
       </BrowserRouter>
     </div>
-  );
-}
+            </AuthenticatedTemplate>
 
-export default App;
+            <UnauthenticatedTemplate>
+                <h5 className="card-title">Please sign-in to access Cloud Collaborators</h5>
+            </UnauthenticatedTemplate>
+        </div>
+    );
+};
+
+export default function App() {
+    return (
+        <PageLayout>
+            <MainContent />
+        </PageLayout>
+    );
+}
---
+++ Collaborator/src/authConfig.js
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { LogLevel } from "@azure/msal-browser";
+
+/**
+ * Configuration object to be passed to MSAL instance on creation. 
+ * For a full list of MSAL.js configuration parameters, visit:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md 
+ */
+export const msalConfig = {
+    auth: {
+        clientId: "ef7f073f-dd38-48fc-9438-4ea36e7e847cß",
+        authority: "https://login.microsoftonline.com/8d09f28d-2b54-4761-98f1-de38762cd939",
+        redirectUri: "http://localhost:3000"
+    },
+    cache: {
+        cacheLocation: "sessionStorage", // This configures where your cache will be stored
+        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+    system: {	
+        loggerOptions: {	
+            loggerCallback: (level, message, containsPii) => {	
+                if (containsPii) {		
+                    return;		
+                }		
+                switch (level) {		
+                    case LogLevel.Error:		
+                        console.error(message);		
+                        return;		
+                    case LogLevel.Info:		
+                        console.info(message);		
+                        return;		
+                    case LogLevel.Verbose:		
+                        console.debug(message);		
+                        return;		
+                    case LogLevel.Warning:		
+                        console.warn(message);		
+                        return;		
+                }	
+            }	
+        }	
+    }
+};
+
+/**
+ * Scopes you add here will be prompted for user consent during sign-in.
+ * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
+ * For more information about OIDC scopes, visit: 
+ * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
+ */
+export const loginRequest = {
+    scopes: ["User.Read"]
+};
+
+/**
+ * Add here the scopes to request when obtaining an access token for MS Graph API. For more information, see:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
+ */
+export const graphConfig = {
+    graphMeEndpoint: "https://graph.microsoft.com/me"
+};
